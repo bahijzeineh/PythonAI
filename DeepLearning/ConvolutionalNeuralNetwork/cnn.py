f2750d12fc09192f55a9e4b1c7acd6f4769f94ce
Bahij Zeineh <bahijz@gmail.com>
2019-07-03 16:02:54 +1000

    cnn ready for test

--- DeepLearning/ConvolutionalNeuralNetwork/cnn.py
+++ DeepLearning/ConvolutionalNeuralNetwork/cnn.py
@@ -13,49 +13,52 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 
-model = Sequential()
-
-model.add(Convolution2D(
-        strides = 1, filters = 32, 
-        kernel_size = 3, 
-        input_shape = (64, 64, 3), 
-        activation = 'relu'))
-
-model.add(MaxPooling2D(pool_size = 2))
-
-model.add(Flatten())
-
-model.add(Dense(units = 128, activation = 'relu'))
-model.add(Dense(units = 1, activation = 'sigmoid'))
-
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
 from keras.preprocessing.image import ImageDataGenerator
 
-train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+class CNN:
+    def __init__(self):
+        self.model = Sequential()
+        
+        self.model.add(Convolution2D(
+                strides = 1, filters = 32, 
+                kernel_size = 3, 
+                input_shape = (64, 64, 3), 
+                activation = 'relu'))
+        
+        self.model.add(MaxPooling2D(pool_size = 2))
+        
+        self.model.add(Flatten())
+        
+        self.model.add(Dense(units = 128, activation = 'relu'))
+        self.model.add(Dense(units = 1, activation = 'sigmoid'))
+        
+        self.model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-test_datagen = ImageDataGenerator(rescale=1./255)
 
-train_generator = train_datagen.flow_from_directory(
-        'dataset/training_set',
-        target_size=(64, 64),
-        batch_size=32,
-        class_mode='binary')
-
-test_generator = test_datagen.flow_from_directory(
-        'dataset/test_set',
-        target_size=(64, 64),
-        batch_size=32,
-        class_mode='binary')
-
-model.fit_generator(
-        train_generator,
-        steps_per_epoch=8000,
-        epochs=20,
-        validation_data=test_generator,
-        validation_steps=2000)
-
+    def fit_gen(self):
+        train_datagen = ImageDataGenerator(
+                rescale=1./255,
+                shear_range=0.2,
+                zoom_range=0.2,
+                horizontal_flip=True)
+        
+        test_datagen = ImageDataGenerator(rescale=1./255)
+        
+        train_generator = train_datagen.flow_from_directory(
+                'dataset/training_set',
+                target_size=(64, 64),
+                batch_size=32,
+                class_mode='binary')
+        
+        test_generator = test_datagen.flow_from_directory(
+                'dataset/test_set',
+                target_size=(64, 64),
+                batch_size=32,
+                class_mode='binary')
+        
+        self.model.fit_generator(
+                train_generator,
+                steps_per_epoch=8000,
+                epochs=3,
+                validation_data=test_generator,
+                validation_steps=2000)
