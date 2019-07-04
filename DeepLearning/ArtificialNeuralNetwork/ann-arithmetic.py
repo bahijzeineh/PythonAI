fdc61e0f16b1dfe35ea8ee504ca4fa21b1676466
Bahij Zeineh <bahijz@gmail.com>
2019-07-04 12:14:43 +1000

    experimental arithmetic ANN

---
+++ DeepLearning/ArtificialNeuralNetwork/ann-arithmetic.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from random import randint
+import numpy as np
+
+opf = [lambda m,n: m + n, lambda m,n: m * n]
+def generateData(size = 100):
+    data = []
+    for i in range(size):
+        x = randint(0,7)
+        y = randint(0,7)
+        opi = randint(0,1)
+        res = opf[opi](x, y)
+        data.append(genBinary(x) + [opi] + genBinary(y) + genBinary(res,64))
+    return data
+def genBinary(x, digits = 8):
+    binary = []
+    while x > 0:
+        mod = int(x) % int(2)
+        binary.append(mod)
+        x -= mod
+        x /= 2
+        digits -= 1
+    for i in range(digits):
+        binary.append(0)
+    return binary[::-1]
+data = np.array(generateData(10000), dtype=np.int32)
+
+X = data[:,0:17]
+y = data[:,17:]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Value Scaling
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+
+model = Sequential()
+
+model.add(Dense(activation = 'relu', units = 32, kernel_initializer = 'uniform', input_dim = 17))
+model.add(Dropout(rate=0.1))
+
+model.add(Dense(activation = 'relu', units = 32, kernel_initializer = 'uniform'))
+model.add(Dropout(rate=0.2))
+
+model.add(Dense(activation = 'sigmoid', units = 64, kernel_initializer = 'uniform'))
+
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# fit ann to training set
+model.fit(X_train, y_train, batch_size = 40, epochs = 80)
+
+y_pred = model.predict(X_test)
+accuracy = (y_pred == y_test)
+print('accuracy: ', (accuracy.sum()/64)/2000)
+
